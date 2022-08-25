20aee9bd757d541d61ad857b7c54edab21dc2498
BronsonBru <bronsonvdbroeck@gmail.com>
2022-08-25 11:55:28 +0200

    t1-t4 commit

---
+++ ToDoList2/Model/TaskFilter.swift
@@ -0,0 +1,20 @@
+//
+//  TaskFilter.swift
+//  ToDoList2
+//
+//  Created by Bronson van den Broeck on 2022/08/24.
+//
+
+import SwiftUI
+
+enum TaskFilter: String{
+    
+    static var allFilters: [TaskFilter]{
+        return [.NonCompleted, .Completed, .OverDue, .All]
+    }
+    
+    case All = "All"
+    case NonCompleted = "To Do"
+    case Completed = "Completed"
+    case OverDue = "Overdue"
+}
